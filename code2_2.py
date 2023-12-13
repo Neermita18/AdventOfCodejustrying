3e05801a7e83a0772a963e25140c3f85e3559da8
Neermita18 <153750505+Neermita18@users.noreply.github.com>
2023-12-14 00:06:52 +0530

    Add files via upload

---
+++ code2_2.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+with open("input2.txt", 'r') as file:
+    lines= file.readlines()
+    ans=0
+    for line in lines:
+        #data= line.split('\n')
+        finalval=1
+        gameno, line= line.split(':')
+        numbers=defaultdict(int)
+        for event in line.split(';'):
+            
+            for balls in event.split(','):
+                
+                number,color = balls.split()
+                numbers[color]=max(int(numbers[color]),int(number)) 
+                  
+        #print(numbers)
+        for i in numbers:
+            v=numbers[i]
+            finalval=finalval*v
+        ans= ans+finalval    
+            
+    print(ans)            
+        
