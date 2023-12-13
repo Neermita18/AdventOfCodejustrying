7acc9cb273d573c5c271b90758c611f5b343b020
Neermita18 <153750505+Neermita18@users.noreply.github.com>
2023-12-13 21:24:24 +0530

    Add files via upload

---
+++ code2.py
@@ -0,0 +1,22 @@
+with open("input2.txt", 'r') as file:
+    lines= file.readlines()
+    ans=0
+    for line in lines:
+        #data= line.split('\n')
+        see= True
+        gameno, line= line.split(':')
+        for event in line.split(';'):
+            for balls in event.split(','):
+                
+                n,color = balls.split()
+                mydict={'red':12, 'green':13, 'blue':14}
+                if int(n) > mydict.get(color,0):
+                    see=False
+        if see:
+            ans+= int((gameno).split()[-1])
+            print(gameno)
+print(ans) #add all gamenos          
+           
+        
+            
+    
