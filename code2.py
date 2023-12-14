ac29f24a11277516943485d80fd78ceba7703f63
Neermita Bhattacharya <153750505+Neermita18@users.noreply.github.com>
2023-12-14 20:25:10 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -4,18 +4,18 @@ with open("input2.txt", 'r') as file:
     for line in lines:
         #data= line.split('\n')
         see= True
-        gameno, line= line.split(':')
-        for event in line.split(';'):
-            for balls in event.split(','):
+        gameno, line= line.split(':') #first element is the gameno, remaining we use later
+        for event in line.split(';'): #events differentiated by ;
+            for balls in event.split(','): #each event has number of balls and color
                 
-                number,color = balls.split()
-                mydict={'red':12, 'green':13, 'blue':14}
-                if int(number) > mydict.get(color,0):
-                    see=False
+                number,color = balls.split() #store those values
+                mydict={'red':12, 'green':13, 'blue':14} #the constraint
+                if int(number) > mydict.get(color,0):  #checking if the number of balls of a color in given event is more than constraint
+                    see=False #can't add if it is
         if see:
-            ans+= int((gameno).split()[-1])
+            ans+= int((gameno).split()[-1]) #gameno contains 'Game : number.' accessing the number
             print(gameno)
-print(ans) #add all gamenos          
+print(ans) #add all gamenos        
            
         
             
